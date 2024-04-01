d265e10f072d955e13f6157e13139a588e52ad04
Devionne24 <littletondevionne4@gmail.com>
2024-04-01 09:41:35 -0400

    Began work on camera functionality

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,24 +6,30 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 10f;
     public float deactivateT = 3f;
+
     // Start is called before the first frame update
+    //Invokes deactivation function
     void Start()
     {
         Invoke("DeactivateGameObject", deactivateT);
     }
 
     // Update is called once per frame
+    //Calls for the bubble to move across the screen
     void Update()
     {
         Move();
     }
 
+    //Moves the bubble along the y-axis
     void Move()
     {
         Vector2 temp = transform.position;
         temp.y += speed * Time.deltaTime;
         transform.position = temp;
     }
+    
+    //Deactivates the bubble to save memory
     void DeactivateGameObject()
     {
         gameObject.SetActive(false);
